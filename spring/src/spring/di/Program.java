e37434d95c6e987f2848386a7afb9294900a5dcf
fandoripsw <qkrtkddn456@naver.com>
2023-04-23 22:03:50 +0900

    test

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -8,7 +8,7 @@ public class Program {
 	public static void main(String[] args) {
 		Exam exam = new NewlecExam();
 //		ExamConsole console = new ExamConsole(exam);
-		console.print();
+		console.print(); 
 		
 	}
 
