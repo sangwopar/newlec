80cc7b360db7866ac3ab710948308308f8295908
fandoripsw <qkrtkddn456@naver.com>
2023-04-23 21:53:47 +0900

    first commit.

---
+++ spring/src/spring/di/Program.java
@@ -0,0 +1,15 @@
+package spring.di;
+
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Exam exam = new NewlecExam();
+		ExamConsole console = new ExamConsole(exam);
+		console.print();
+		
+	}
+
+}
